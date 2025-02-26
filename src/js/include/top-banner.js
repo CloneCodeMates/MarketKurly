762e84d18a5f320885402143cdfdf1a0203906d8
윤진 <151013932+maruru301@users.noreply.github.com>
2025-02-26 17:05:29 +0900

    feat: includeHtml() 실행 후 JS 파일이 동작하도록 수정 #39

--- src/js/include/top-banner.js
+++ src/js/include/top-banner.js
@@ -1,6 +1,8 @@
-const closeButton = document.getElementById('top-banner__close-button');
-const topBanner = document.getElementById('top-banner');
+includeHtml().then(() => {
+    const closeButton = document.getElementById('top-banner__close-button');
+    const topBanner = document.getElementById('top-banner');
 
-closeButton.addEventListener('click', () => {
-    topBanner.style.display = 'none';
+    closeButton.addEventListener('click', () => {
+        topBanner.style.display = 'none';
+    });
 });
