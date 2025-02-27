e96229b915670b9f618b177513d1a324ccd84e8d
윤진 <151013932+maruru301@users.noreply.github.com>
2025-02-27 23:46:38 +0900

    feat: 버튼 클릭 시 Swiper 슬라이드가 해당 방향으로 이동하도록 구현 #46

--- src/js/include/event.js
+++ src/js/include/event.js
@@ -1,1 +1,10 @@
-includeHtml().then(() => {});
+includeHtml().then(() => {
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: 1,
+        loop: true,
+        navigation: {
+            nextEl: '#event-next-button',
+            prevEl: '#event-prev-button',
+        },
+    });
+});
