ec9bf89f1324ec59ae4c8676bbe69c1532988d18
윤진 <151013932+maruru301@users.noreply.github.com>
2025-02-28 20:48:31 +0900

    feat: 자동 슬라이드로 변경 (3초마다) #46

--- src/js/include/event.js
+++ src/js/include/event.js
@@ -1,22 +1,25 @@
 includeHtml().then(() => {
     const swiper = new Swiper('.swiper', {
         slidesPerView: 1, // 한 번에 보여줄 슬라이드 개수
-        loop: true, // 슬라이드를 무한 반복하도록 설정
+        loop: true, // 슬라이드 무한 반복
         navigation: {
             nextEl: '#event-next-button',
             prevEl: '#event-prev-button',
         },
         pagination: {
             el: '#event-page-index',
-            type: 'custom', // 커스터마이즈된 페이지 번호 형식을 사용
-            renderCustom: function (swiper, current, total) {
+            type: 'custom',
+            renderCustom: (swiper, current, total) => {
                 return `${current} / ${total}`; // 커스터마이즈된 페이지 번호 형식
             },
         },
+        autoplay: {
+            delay: 3000, // 3초마다 슬라이드 자동 전환
+            disableOnInteraction: false,
+        },
         on: {
-            // Swiper 초기화 시 호출되는 함수
-            init: function () {
-                updatePageIndex(swiper); // 초기 페이지 번호 업데이트
+            init: () => {
+                swiper.update();
             },
         },
     });
