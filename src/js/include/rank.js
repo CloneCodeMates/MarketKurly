d5a803d9389e68787da9534c480c32a8d86d1386
윤진 <151013932+maruru301@users.noreply.github.com>
2025-03-05 14:31:59 +0900

    feat: swiper 적용중 #57

--- src/js/include/rank.js
+++ src/js/include/rank.js
@@ -1,1 +1,17 @@
-includeHtml().then(() => {});
+includeHtml().then(() => {
+    const rankSwiper = document.querySelector('#ranking-products .swiper-container');
+
+    if (!rankSwiper) {
+        console.log('rank Swiper 를 찾을 수 없습니다.');
+        return;
+    }
+
+    new Swiper(rankSwiper, {
+        slidesPerView: 'auto',
+        slidesPerGroup: 5, // 버튼 클릭 시 이동하는 슬라이드 개수
+        navigation: {
+            nextEl: '#rank-next-button',
+            prevEl: '#rank-prev-button',
+        },
+    });
+});
