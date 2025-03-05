9302dc887f9e04c0dca72fa6fa0bd0ab466ece0f
윤진 <151013932+maruru301@users.noreply.github.com>
2025-03-05 17:13:19 +0900

    feat: 평일은 24시간, 주말은 48시간으로 설정 #49

--- src/js/include/time.js
+++ src/js/include/time.js
@@ -1,11 +1,16 @@
 includeHtml().then(() => {
     function startCountdown() {
         const timeUnits = document.querySelectorAll('#countdown-timer .time-units span');
-        let deadline = localStorage.getItem('weekendSaleDeadline');
+        let deadline = localStorage.getItem('saleDeadline');
+
+        const now = new Date();
+        const today = now.getDate(); // 0(일요일) ~ 6(토요일)
+        const isWeekend = today === 0 || today === 6; // 주말 여부
+        const countdownHours = isWeekend ? 48 : 24; // 주말이면 48시간, 평일이면 24시간
 
         if (!deadline) {
-            deadline = new Date().getTime() + 48 * 60 * 60 * 1000; // 48시간 설정
-            localStorage.setItem('weekendSaleDeadline', deadline);
+            deadline = now.getTime() + countdownHours * 60 * 60 * 1000; // 시간 설정
+            localStorage.setItem('saleDeadline', deadline);
         } else {
             deadline = parseInt(deadline, 10);
         }
@@ -19,7 +24,7 @@ includeHtml().then(() => {
                 timeUnits[1].textContent = '00';
                 timeUnits[2].textContent = '00';
                 clearInterval(timer);
-                localStorage.removeItem('weekendSaleDeadline'); // 카운트다운 종료 후 초기화
+                localStorage.removeItem('saleDeadline'); // 카운트다운 종료 후 초기화
                 return;
             }
 
